dba65a179afd10f5d6ab4cc103c00000b41f556b
Afitria Sopiyanti <93569904+afitria@users.noreply.github.com>
2022-01-09 21:28:35 +0700

    Add files via upload

---
+++ views/main.blade.php
@@ -0,0 +1,385 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+  <title>Dapur Mama</title>
+  <meta content="" name="description">
+  <meta content="" name="keywords">
+
+  <!-- Favicons -->
+  <link href="assets/img/favicon.png" rel="icon">
+  <link href="assets/img/apple-touch-icon.png" rel="apple-touch-icon">
+
+  <!-- Google Fonts -->
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Playfair+Display:ital,wght@0,400;0,500;0,600;0,700;1,400;1,500;1,600;1,700|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+
+  <!-- Vendor CSS Files -->
+  <link href="assets/vendor/animate.css/animate.min.css" rel="stylesheet">
+  <link href="assets/vendor/aos/aos.css" rel="stylesheet">
+  <link href="{{asset('resto/assets/vendor/bootstrap/css/bootstrap.min.css')}}" rel="stylesheet">
+  <link href="{{asset('resto/assets/vendor/bootstrap-icons/bootstrap-icons.css')}}" rel="stylesheet">
+  <link href="{{asset('resto/assets/vendor/boxicons/css/boxicons.min.css')}}" rel="stylesheet">
+  <link href="{{asset('resto/assets/vendor/glightbox/css/glightbox.min.css')}}" rel="stylesheet">
+  <link href="{{asset('resto/assets/vendor/swiper/swiper-bundle.min.css')}}" rel="stylesheet">
+
+  <!-- Template Main CSS File -->
+  <link href="{{asset('resto/assets/css/style.css')}}" rel="stylesheet">
+
+  <!-- =======================================================
+  * Template Name: Restaurantly - v3.7.0
+  * Template URL: https://bootstrapmade.com/restaurantly-restaurant-template/
+  * Author: BootstrapMade.com
+  * License: https://bootstrapmade.com/license/
+  ======================================================== -->
+</head>
+
+<body>
+
+  <!-- ======= Top Bar ======= -->
+ 
+
+  <!-- ======= Header ======= -->
+  <header id="header" class="fixed-top d-flex align-items-cente">
+    <div class="container-fluid container-xl d-flex align-items-center justify-content-lg-between">
+
+      <h1 class="logo me-auto me-lg-0"><a href="index.html">Dapur Mama</a></h1>
+      <!-- Uncomment below if you prefer to use an image logo -->
+      <!-- <a href="index.html" class="logo me-auto me-lg-0"><img src="assets/img/logo.png" alt="" class="img-fluid"></a>-->
+
+      <nav id="navbar" class="navbar order-last order-lg-0">
+        <ul>
+          <li><a class="nav-link scrollto" href="#why-us">Resep</a></li>
+          <li><a class="nav-link scrollto" href="#gallery">Gallery</a></li>
+        </ul>
+        <i class="bi bi-list mobile-nav-toggle"></i>
+      </nav><!-- .navbar -->
+
+    </div>
+  </header><!-- End Header -->
+
+  <!-- ======= Hero Section ======= -->
+<br><br><br>
+  <main id="main">
+
+    <!-- ======= About Section ======= -->
+
+    <!-- ======= Why Us Section ======= -->
+    <section id="why-us" class="why-us">
+      <div class="container" data-aos="fade-up">
+
+        <div class="section-title">
+          <p>Resep Masakan</p>
+        </div>
+
+        <div class="row">
+
+          <div class="col-lg-4">
+            <div class="box" data-aos="zoom-in" data-aos-delay="100">
+              <span>01</span>
+              <h4>Karedok</h4>
+              <h3>Bahan</h3>
+ <p>
+    100 g taoge;
+    100 g kacang panjang, iris;
+    75 g kol, iris;
+    1 ikat daun kemangi, petik;
+    2 buah terung lalap, iris;
+    2 buah mentimun, iris;
+</p>
+<h3>Bumbu halus</h3>
+175 g kacang tanah, buang kulit dan goreng;
+    45 g gula merah;
+    1 siung bawang putih;
+    3 buah cabai rawit merah;
+    3 cm kencur;
+    2 sdm air asam jawa;
+    1 sdm Bango Kecap Manis Light;
+    1 sdt garam;
+    100 ml air;
+
+    <h3>Cara membuat</h3>
+    1. Haluskan bumbu dan pindahkan bumbu halus ke dalam mangkuk besar.
+Menuang Bango Kecap Manis Light dalam bumbu.
+2. Tambahkan air ke dalam mangkuk, lalu tuangkan Bango Kecap Manis Light  dan aduk hingga bumbu tercampur rata.
+karedok MAHI
+3. Masukkan semua sayuran. Aduk hingga rata dan hidangkan di piring saji.
+
+            </div>
+          </div>
+
+          <div class="col-lg-4 mt-4 mt-lg-0">
+            <div class="box" data-aos="zoom-in" data-aos-delay="200">
+              <span>02</span>
+              <h4>Tutug Oncom </h4>
+              <h3>Bahan</h3>
+
+              600 g nasi putih;
+    400 g oncom, iris;
+    2 sdm Bango Kecap Manis Light;
+    2 sdm Royco Kaldu Jamur;
+    ½ sdt merica putih bubuk;
+    1 sdm terasi, bakar;
+    3 sdm minyak;
+    daun pisang, untuk membungkus	;
+    
+    <h3>Bumbu halus</h3>
+    7 butir bawang merah;
+    6 siung bawang putih;
+    3 cm kencur;
+    6 buah cabai keriting merah;
+    5 buah cabai rawit merah;
+
+    <h3>Cara membuat</h3>
+    1. Panaskan 1 sdm minyak, masak oncom sambil diaduk hingga aroma langunya berkurang. nasi tutug oncom tumis bumbu
+2. Panaskan sisa minyak, tumis bumbu halus hingga harum. Masukkan oncom, Royco Kaldu Jamur, Bango Kecap Manis Light, merica, dan terasi. Masak hingga matang dan tumisan mengering.
+nasi tutug oncom dan nasi
+3. Tambahkan nasi putih, aduk hingga rata. Angkat. Bagi menjadi 4 bagian. Sisihkan. nasi tutug oncom daun pisang
+4. Siapkan daun pisang, olesi dengan minyak. Sisihkan. nasi tutug oncom bungkus daun
+5. Taruh nasi ke atas daun pisang. Bungkus. Sematkan dengan tusuk gigi. nasi tutug oncom produk
+6. Sajikan nasi tutug oncom dengan pelengkap.
+
+
+            </div>
+          </div>
+
+          <div class="col-lg-4 mt-4 mt-lg-0">
+            <div class="box" data-aos="zoom-in" data-aos-delay="300">
+              <span>03</span>
+              <h4>Empal Gentong</h4>
+              <h3>Bahan</h3>
+              250 gr daging sapi ;
+              100 gr paru ;
+              100 gr babat;
+
+              <h3>Bumbu halus</h3>
+              100 gr bawang merah ;
+              150 gr bawang putih ;
+              1 ruas kunyit 60 gr kemiri;
+               100 ml santan;
+                3 lembar daun salam ;
+                3 batang serai;
+                 1 ruas lengkuas;
+                  3 lembar daun jeruk;
+                   Garam, merica, dan gula pasir secukupnya;
+
+<h3>Cara membuat</h3>
+1. Rebus daging, paru, dan babat bersama garam dan rempah daun salam, serai, daun jeruk, dan lengkuas untuk menghilangkan bau amis. Rebus daging sekitar 45 menit sampai matang dan empuk.
+
+2. Setelah daging dan jeroan empuk dan matang, angkat daging. Potong-potong berbentuk kotak kecil. Sisihkan.
+ 3. Haluskan bawang merah, bawang putih, kemiri, dan kunyit. Kemudian tumis dengan sedikit minyak sampai harum.
+ 4.Setelah bumbu harum, tambahkan daun salam, serai, lengkuas, dan daun jeruk nipis. Kemudian masukkan air atau kaldu sapi. Tambahkan garam, merica, dan gula. 
+5. Masukkan daging, babat, dan paru ke dalam kuah bumbu kunig. Kemudian tambahkan santan. Masak dengan api kecil sambil diaduk agar santan tidak pecah. 
+6. Masak sekitar 10 menit dan empal gentong siap disajikan bersama nasi hangat atau lontong.
+
+
+            </div>
+          </div>
+
+          <div class="col-lg-4 mt-4 mt-lg-0">
+            <div class="box" data-aos="zoom-in" data-aos-delay="300">
+              <span>04</span>
+              <h4>Mie Kocok</h4>
+              <h3>Bahan</h3>
+              1 kg mie kuning;
+250 g kikil sapi rebus;
+250 g daging tetelan sapi rebus;
+20 butir bakso sapi;
+200 g tauge;
+Kuah:
+2 liter kaldu sapi;
+3 lembar daun salam;
+4 cm lengkuas, memarkan;
+1 batang daun bawang, iris kasar;
+Bumbu, haluskan:
+8 siung bawang putih;
+5 butir bawang merah;
+3 butir kemiri;
+1 sdt merica butiran;
+1 cm jahe;
+1 sdm garam;
+Pelengkap:
+seledri cincang;
+bawang merah goreng;
+jeruk limau;
+sambal rawit merah;
+kerupuk kanji;
+
+<h3>Cara membuat</h3>
+Kuah: 
+1. Giling semua bahan bumbu hingga halus benar.
+2. Panaskan 4 sdm minyak dalam wajan. Tumis bumbu halus bersama daun salam dan lengkuas hingga harum dan matang.
+3. Masukkan bumbu ke dalam panci, tambahkan kaldu.
+4. Masak dengan api sedang hingga mendidih. Masukkan bakso, tetelan, kikil sapi dan daun bawang.
+5. Siram mie kuning dengan air panas lalu tiriskan.
+6. Seduh tauge sebentar dengan air mendidih dan tiriskan.
+
+Penyajian:
+Susun mie kuning, tauge, dan siram kaldu berikut bakso, kikil dan tetelan.
+Sajikan panas dengan Pelengkapnya.
+
+            </div>
+          </div>
+
+          <div class="col-lg-4 mt-4 mt-lg-0">
+            <div class="box" data-aos="zoom-in" data-aos-delay="300">
+              <span>05</span>
+              <h4> Gepuk</h4>
+              <h3>Bahan</h3>
+              500 g daging has sapi
+4 cm lengkuas parut
+2 lembar daun salam
+2 sdm kecap manis
+1 sdm gula Jawa serut halus
+1/2 sdt asam Jawa
+500 ml santan segar
+1 sdm kaldu bubuk
+Bumbu Halus:
+7 butir bawang merah
+5 siung bawang putih
+1 sdm ketumbar sangrai
+3 butir kemiri
+2 sdt garam
+Taburan:
+bawang merah goreng
+
+<h3>Cara membuat</h3>
+1. Didihkan air secukupnya lalu rebus daging hingga mendidih beberapa saat. Angkat daging dan buang air perebusnya.
+2. Rebus air penggantinya dalam panci, rebus daging hingga empuk. Angkat dan sisihkan.
+3. Potong daging searah dgn serat seteal 3/4 cm, pukul pukul dengan ulekan atau pemukul daging hingga daging melebar dan empuk.
+4. Masukkan daging beserta Bumbu Halus ke dalam wajan bersama sisa air perebusnya.
+5. Tambahkan kaldu bubuk, daun salam, lengkuas parut, asam Jawa, gula merah, kecap dan santan.
+6. Masak hingga air menyusut dan bumbu meresap lalu angkat daging.
+7. Siapkan wajan bersih, goreng daging dengan api yang kecil hingga berwarna cokelat keemasan.
+8. Angkat dan taburi bawang merah goreng.
+
+
+              
+
+            </div>
+          </div>
+
+          <div class="col-lg-4 mt-4 mt-lg-0">
+            <div class="box" data-aos="zoom-in" data-aos-delay="300">
+              <span>06</span>
+              <h4>Ayam Bakakak</h4>
+              <h3>Bahan</h3>
+              1 ekor ayam kampung, belah bagian tengah tidak putus;
+    4 sdm Bango Kecap Manis;
+    2 butir kemiri;
+    ¼ sdt merica putih bubuk;
+    12 butir bawang merah;
+    2 siung bawang putih;
+    4 buah cabai merah besar;
+    ½ sdm ketumbar;
+    2 cm jahe;
+    2 cm kunyit;
+    1 sdm garam;
+
+    <h3>Cara membuat</h3>
+    
+1. Haluskan kemiri, bawang merah, bawang putih, cabai merah, jahe, dan kunyit. Masukkan bumbu yang sudah dihaluskan ke dalam wadah. Tambahkan garam ketumbar, merica, dan Bango Kecap Manis. Aduk rata.
+2. Masukkan ayam yang telah dibersihkan. Baluri seluruh permukaan ayam dengan bumbu.Diamkan selama satu jam di dalam kulkas.
+3. Siapkan pembakaran. Panggang ayam yang telah dibumbui. Gunakan sisa bumbu marinasi sebagai alat olesan.
+4. Olesi sesekali seuruh permukaan ayam. Angkat setelah matang.
+5. Hidangkan selagi hangat dengan nasi dan sambal.
+
+            </div>
+          </div>
+
+        </div>
+
+      </div>
+    </section><!-- End Why Us Section -->
+    <section id="gallery" class="gallery">
+
+<div class="container" data-aos="fade-up">
+  <div class="section-title">
+    <h2>Gallery</h2>
+  </div>
+</div>
+
+<div class="container-fluid" data-aos="fade-up" data-aos-delay="100">
+
+  <div class="row g-0">
+
+    <div class="col-lg-3 col-md-4">
+      <div class="gallery-item">
+        <a href="{{asset('resto/assets/img/gallery/karedok.jpeg')}}" class="gallery-lightbox" data-gall="gallery-item">
+          <img src="{{asset('resto/assets/img/gallery/karedok.jpeg')}}" alt="" class="img-fluid">
+        </a>
+      </div>
+    </div>
+
+    <div class="col-lg-3 col-md-4">
+      <div class="gallery-item">
+        <a href="{{asset('resto/assets/img/gallery/oncom.jpeg')}}" class="gallery-lightbox" data-gall="gallery-item">
+          <img src="{{asset('resto/assets/img/gallery/oncom.jpeg')}}" alt="" class="img-fluid">
+        </a>
+      </div>
+    </div>
+
+    <div class="col-lg-3 col-md-4">
+      <div class="gallery-item">
+        <a href="{{asset('resto/assets/img/gallery/gentong.jpeg')}}" class="gallery-lightbox" data-gall="gallery-item">
+          <img src="{{asset('resto/assets/img/gallery/gentong.jpeg')}}" alt="" class="img-fluid">
+        </a>
+      </div>
+    </div>
+
+    <div class="col-lg-3 col-md-4">
+      <div class="gallery-item">
+        <a href="{{asset('resto/assets/img/gallery/mikocok.jpeg')}}" class="gallery-lightbox" data-gall="gallery-item">
+          <img src="{{asset('resto/assets/img/gallery/mikocok.jpeg')}}" alt="" class="img-fluid">
+        </a>
+      </div>
+    </div>
+
+    <div class="col-lg-3 col-md-4">
+      <div class="gallery-item">
+        <a href="{{asset('resto/assets/img/gallery/gepuk.jpeg')}}" class="gallery-lightbox" data-gall="gallery-item">
+          <img src="{{asset('resto/assets/img/gallery/gepuk.jpeg')}}" alt="" class="img-fluid">
+        </a>
+      </div>
+    </div>
+
+    <div class="col-lg-3 col-md-4">
+      <div class="gallery-item">
+        <a href="{{asset('resto/assets/img/gallery/bakakak.jpeg')}}" class="gallery-lightbox" data-gall="gallery-item">
+          <img src="{{asset('resto/assets/img/gallery/bakakak.jpeg')}}" alt="" class="img-fluid">
+        </a>
+      </div>
+    </div>
+
+    
+
+  </div>
+
+</div>
+</section><!-- End Gallery Section -->
+
+
+  </main><!-- End #main -->
+
+  <!-- ======= Footer ======= -->
+  
+
+  <div id="preloader"></div>
+  <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
+
+  <!-- Vendor JS Files -->
+  <script src="assets/vendor/aos/aos.js"></script>
+  <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="assets/vendor/glightbox/js/glightbox.min.js"></script>
+  <script src="assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
+  <script src="assets/vendor/swiper/swiper-bundle.min.js"></script>
+  <script src="assets/vendor/php-email-form/validate.js"></script>
+
+  <!-- Template Main JS File -->
+  <script src="{{asset('resto/assets/js/main.js')}}"></script>
+
+</body>
+
+</html>
